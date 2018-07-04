c98553fb322d7a471a35a88fedfb0f4edb652e66
Nahuel SG <nahuelsg.64@gmail.com>
2018-07-04 00:06:56 -0300

    Carousel / Photo display Like pic Timeago

--- src/app/gallery/carousel/carousel.component.ts
+++ src/app/gallery/carousel/carousel.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material';
+import {ChangeDetectorRef, Component, Inject, NgZone, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {Picture} from '../../shared/domain/picture';
+import {APIService} from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-carousel',
@@ -10,16 +11,43 @@ import {Picture} from '../../shared/domain/picture';
 export class CarouselComponent implements OnInit {
 
   pics: Picture[] = [];
-  currentActive: number;
+  currentActive: string;
   serverURL: string;
+  likeBtnClass: string;
+  likeBtnIcon: string;
+  likeBtnText: string;
 
-  constructor(@Inject(MAT_DIALOG_DATA) data) {
+  constructor(public dialogRef: MatDialogRef<CarouselComponent>,
+              @Inject(MAT_DIALOG_DATA) data,
+              private apiService: APIService,
+              public zone: NgZone,
+              public ref: ChangeDetectorRef) {
     this.pics = data.pics;
     this.currentActive = data.activeId;
     this.serverURL = data.serverURL;
+    this.likeBtnClass = 'btn-default';
+    this.likeBtnIcon = 'nc-tap-01';
+    this.likeBtnText = '   LIKE';
+    this.dialogRef.backdropClick().subscribe(() => this.close());
   }
 
   ngOnInit() {
+    this.zone.run(() => {
+      this.ref.detectChanges();
+    });
+  }
+
+  public likePic(pic) {
+    this.likeBtnIcon = 'nc-satisfied';
+    this.likeBtnClass = 'btn-info';
+    this.likeBtnText = '   Me gusta!';
+    this.apiService.likePic(pic._id).subscribe((res: Picture) => {
+      pic.likesCount++;
+    });
+  }
+
+  close() {
+    this.dialogRef.close(this.pics);
   }
 
 }
